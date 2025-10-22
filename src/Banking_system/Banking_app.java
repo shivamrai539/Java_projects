1b296b8d0bb0cab20fd48fb48a218f9e24b828a8
Shivam Rai <shivamrai70049@gmail.com>
2025-10-22 21:49:37 +0530

    Update database password in Banking_app.java

--- src/Banking_system/Banking_app.java
+++ src/Banking_system/Banking_app.java
@@ -11,7 +11,7 @@ public class Banking_app {
 
     private static final String username = "root";
 
-    private static final String password = "You-Password";
+    private static final String password = "Your-Password";
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         try{
